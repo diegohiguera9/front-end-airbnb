ca5a0201b37c1fc4a178dd9310a4b01f267c90cd
Diego <drhiguerag@unal.edu.co>
2022-09-15 18:30:41 -0500

    pop over on search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,26 +4,26 @@ import searchIcon from '../styles/icons/searchIcon.svg';
 const SearchBar = (props)=>{
     return(
     <div className="header__search">
-        <button>
+        <div className='header__search__button'>
             <div className="header__search__cualquier">
                 {props.first}
             </div>
-        </button>   
+        </div>   
         <span></span>
-        <button>
+        <div className='header__search__button'>
             <div className="header__search__semana">
             {props.second}
             </div>
-        </button>
+        </div>
         <span></span>
-        <button>
+        <div className='header__search__button'>
             <div className="header__search__cuantos" >
                 {props.third}
             </div>
             <div className="header__searchIcon">
                 <img src={searchIcon} alt="Search Icon" />
             </div>
-        </button>
+        </div>
     </div>
     )
 }
