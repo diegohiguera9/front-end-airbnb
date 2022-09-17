54f960af3641a136b9df3624baf4ed511ca46363
Jhon Vasquez <jhonv.v1@gmail.com>
2022-09-16 23:51:51 -0500

    implementacion redux rent element funcional

--- src/components/RentCalendar.js
+++ src/components/RentCalendar.js
@@ -5,55 +5,55 @@ import AmenitieTag from "./AmenitieTag";
 import { RangeCalendar } from "@mantine/dates";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { changeDate } from "../store/reducer/calendarReducer";
+import { changeDate, changeTitle, changeDateHead } from "../store/reducer/calendarReducer";
 
 const RentCalendar = () => {
-  const rentCalendarOne = useSelector((state)=>state.calendarReducer.dates);
-  const dispatch = useDispatch()
-  
+  const rentCalendarOne = useSelector((state) => state.calendarReducer.dates);
+  const dispatch = useDispatch();
+  const dateTitle = useSelector((state) => state.calendarReducer.title);
+  const dateHeader= useSelector((state)=> state.calendarReducer.head)
   //const [rentCalendarOne, setRentCalendarOne] = useState([null,null]);
-  const [dateTitle, setDateTitle] = useState("")
-  const [dateHeader, setDateHeader] = useState("");
-  
+  //const [dateTitle, setDateTitle] = useState("")
+  //const [dateHeader, setDateHeader] = useState("");
 
   const clearCalendar = () => {
-    dispatch(changeDate([null,null]))
+    dispatch(changeDate([null, null]));
   };
   const textDateHeader = () => {
     if (rentCalendarOne[0] === null && rentCalendarOne[1] === null) {
-      setDateTitle("Select check-in date");
-      setDateHeader("Add your travel dates for exact pricing");
-      
+      dispatch(changeTitle("Select check-in date"));
+      dispatch(changeDateHead("Add your travel dates for exact pricing"));
     } else if (rentCalendarOne[0] && rentCalendarOne[1] === null) {
-      setDateTitle("Select checkout date");
-      setDateHeader("Minimum stay: 2 nights");
+      dispatch(changeTitle("Select checkout date"));
+      dispatch(changeDateHead("Minimum stay: 2 nights"));
     } else if (rentCalendarOne[0] && rentCalendarOne[1]) {
-            if(rentCalendarOne[0] && rentCalendarOne[1]){
-              let rentDates = rentCalendarOne[1].getTime() - rentCalendarOne[0].getTime();
-              rentDates = rentDates / (1000 * 3600 * 24);
-              setDateTitle(`${rentDates} nights in Bogota`);
-            }
-            const sDates = rentCalendarOne.map((item) => {
+      if (rentCalendarOne[0] && rentCalendarOne[1]) {
+        let rentDates =
+          rentCalendarOne[1].getTime() - rentCalendarOne[0].getTime();
+        rentDates = rentDates / (1000 * 3600 * 24);
+        dispatch(changeTitle(`${rentDates} nights in Bogota`));
+      }
+      const sDates = rentCalendarOne.map((item) => {
         return [`${item.getMonth()}, ${item.getDate()}, ${item.getFullYear()}`];
       });
-      return setDateHeader(`${sDates[0]} - ${sDates[1]}`);
+      return dispatch(changeDateHead(`${sDates[0]} - ${sDates[1]}`));
     }
   };
   useEffect(() => {
-   textDateHeader();
-  },[rentCalendarOne]);
-  
+    textDateHeader();
+  }, [rentCalendarOne]);
+
   return (
-    <div >
-      <div className="rentCalendar" >
-        <div className="dateTitle" children={dateTitle}/>
-        <div className="dateHeader" children={dateHeader}/>
+    <div>
+      <div className="rentCalendar">
+        <div className="dateTitle" children={dateTitle} />
+        <div className="dateHeader" children={dateHeader} />
         <div>
           <RangeCalendar
             minDate={new Date()}
             amountOfMonths={2}
             value={rentCalendarOne}
-            onChange={(e)=>dispatch(changeDate(e))}
+            onChange={(e) => dispatch(changeDate(e))}
             styles={(theme) => ({
               day: {
                 "&[data-selected]": {
--- src/store/reducer/calendarReducer.js
+++ src/store/reducer/calendarReducer.js
@@ -1,5 +1,7 @@
 
 export const CHANGE_DATE = "CHANGE_DATE"
+export const CHANGE_TITLEDATE = "CHANGE_TITLEDATE"
+export const CHANGE_HEADDATE = "CHANGE_HEADDATE"
 
 
 //action creator
@@ -10,18 +12,43 @@ export const changeDate = (value) => {
     }
 }
 
+ export const changeTitle=(value)=>{
+    return{
+        type: CHANGE_TITLEDATE,
+        payload: value
+    }
+ }
+ export const changeDateHead=(value)=>{
+    return{
+        type: CHANGE_HEADDATE,
+        payload: value
+    }
+ }
 
+// state
 const initialState = {
     dates: [null,null],
+    title: "",
+    head: ""
 }
 
-
+//reducer
 const calendarReducer = (state = initialState, action) => {
     switch (action.type) {
         case CHANGE_DATE:
             return {
                 ...state,
                 dates: action.payload
+            }
+            case CHANGE_TITLEDATE:
+            return {
+                ...state,
+                title: action.payload
+            }
+            case CHANGE_HEADDATE:
+            return {
+                ...state,
+                head: action.payload
             }
         default:
             return state
