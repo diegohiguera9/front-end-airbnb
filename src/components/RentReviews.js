0b562563875f2fbb2664b28bd767249f4f3f103b
Jhon Vasquez <jhonv.v1@gmail.com>
2022-09-20 17:38:21 -0500

    correccion stycky

---
+++ src/components/RentReviews.js
@@ -0,0 +1,11 @@
+const RentReviews = () => {
+  return (
+    <div className="reviewsContainer">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default RentReviews;
