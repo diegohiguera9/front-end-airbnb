974e19a736098e0844f35c294a6f30a73abdfeb4
Jhon Vasquez <jhonv.v1@gmail.com>
2022-09-21 11:54:02 -0500

    creacion ReviewScore

--- src/components/RentReviews.js
+++ src/components/RentReviews.js
@@ -1,8 +1,13 @@
+import ReviewsPuntuation from "./ReviewsPuntuation";
+import ReviewScore from "./ReviewScore";
+import "../styles/components/rentReviews.scss"
+import RentComments from "./RentComments";
 const RentReviews = () => {
   return (
     <div className="reviewsContainer">
-      <div></div>
-      <div></div>
+      <ReviewsPuntuation rating={"1.11"} reviews={"10 Reviews"}/>
+      <ReviewScore />
+      <RentComments/>
       <div></div>
     </div>
   );
