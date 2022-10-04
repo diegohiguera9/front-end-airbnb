0bc05d847134b132cba57e7218e0c9ad33183782
Ruben Dario Castilla Romero <67343176+CastRome@users.noreply.github.com>
2022-10-03 22:08:56 -0500

    fix login

--- src/components/ModalLogin.js
+++ src/components/ModalLogin.js
@@ -1,29 +1,42 @@
-import "../styles/components/ModalLogin.scss"
-import ButtonImg from "./ButtonImg";
+import '../styles/components/ModalLogin.scss';
+import ButtonImg from './ButtonImg';
 const ModalLogin = () => {
-
-    return (
-        <>
-            <div className="containerLogin">
-
-                <p>Iniciar sesión o registrate</p>
-                <hr />
-                <h1 >Te damos la bienvenida a Airbnb</h1>
-                <div className="wrapper">
-                    <span for="email">Correo electrónico</span>
-                    <input className="loginEmail" id="email" type="email" placeHolder="Correo electrónico"></input>
-                </div>
-                <button className="aceptarButton"> Continúa </button>
-                <p>o</p>
-                <ButtonImg clase={"buttonLoginType facebook"} texto={"Continúa con Facebook"} />
-                <ButtonImg clase={"buttonLoginType google"} texto={"Continúa con Google"} />
-                <ButtonImg clase={"buttonLoginType apple"} texto={"Continúa con Apple"} />
-                <ButtonImg clase={"buttonLoginType telefono"} texto={"Continúa con un teléfono"} />
-
-            </div>
-        </ >
-    )
-
-}
+  return (
+    <>
+      <div className="containerLogin">
+        <p>Iniciar sesión o registrate</p>
+        <hr />
+        <h1>Te damos la bienvenida a Airbnb</h1>
+        <div className="wrapper">
+          <span htmlFor="email">Correo electrónico</span>
+          <input
+            className="loginEmail"
+            id="email"
+            type="email"
+            placeholder="Correo electrónico"
+          ></input>
+        </div>
+        <button className="aceptarButton"> Continúa </button>
+        <p>o</p>
+        <ButtonImg
+          clase={'buttonLoginType facebook'}
+          texto={'Continúa con Facebook'}
+        />
+        <ButtonImg
+          clase={'buttonLoginType google'}
+          texto={'Continúa con Google'}
+        />
+        <ButtonImg
+          clase={'buttonLoginType apple'}
+          texto={'Continúa con Apple'}
+        />
+        <ButtonImg
+          clase={'buttonLoginType telefono'}
+          texto={'Continúa con un teléfono'}
+        />
+      </div>
+    </>
+  );
+};
 
 export default ModalLogin;
