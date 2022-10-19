85cb46f37255cab2160af31463d6fc78d5a8ad64
Diego <drhiguerag@unal.edu.co>
2022-10-19 13:22:55 -0500

    changes to home listings

--- src/components/CardHomeContainer.js
+++ src/components/CardHomeContainer.js
@@ -1,56 +1,88 @@
 import "../styles/components/CardHomeContainer.scss";
 import CardHomeHost from "./CardHomeHost";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const CardHomeContainer = () => {
-  const data = [
-    {
-      name: "Ruben",
-      fechas: "1-2 Oct",
-      img: process.env.PUBLIC_URL + "cardhome/1.webp",
-      huespedes: 16,
-      llegada: "14 Oct 2022",
-      salida: "17 Oct 2022",
-      fecha: "25 Sept 2022",
-      codigo: "HM22ASCE42",
-    },
-    {
-      name: "Diego",
-      fechas: "1-2 Oct",
-      img: process.env.PUBLIC_URL + "cardhome/1.webp",
-      huespedes: 16,
-      llegada: "14 Oct 2022",
-      salida: "17 Oct 2022",
-      fecha: "25 Sept 2022",
-      codigo: "HM22ASCE42",
-    },
-    {
-      name: "John",
-      fechas: "1-2 Oct",
-      img: process.env.PUBLIC_URL + "cardhome/1.webp",
-      huespedes: 16,
-      llegada: "14 Oct 2022",
-      salida: "17 Oct 2022",
-      fecha: "25 Sept 2022",
-      codigo: "HM22ASCE42",
-    },
-  ];
+  const [reservations, setReservations] = useState("");
+  const token = localStorage.getItem("token");
+
+  const getReservations = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://airbnbclonetop24.herokuapp.com/reservations/showHost",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setReservations(data.data);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    getReservations();
+  }, []);
+
+  // const data = [
+  //   {
+  //     name: "Ruben",
+  //     fechas: "1-2 Oct",
+  //     img: process.env.PUBLIC_URL + "cardhome/1.webp",
+  //     huespedes: 16,
+  //     llegada: "14 Oct 2022",
+  //     salida: "17 Oct 2022",
+  //     fecha: "25 Sept 2022",
+  //     codigo: "HM22ASCE42",
+  //   },
+  //   {
+  //     name: "Diego",
+  //     fechas: "1-2 Oct",
+  //     img: process.env.PUBLIC_URL + "cardhome/1.webp",
+  //     huespedes: 16,
+  //     llegada: "14 Oct 2022",
+  //     salida: "17 Oct 2022",
+  //     fecha: "25 Sept 2022",
+  //     codigo: "HM22ASCE42",
+  //   },
+  //   {
+  //     name: "John",
+  //     fechas: "1-2 Oct",
+  //     img: process.env.PUBLIC_URL + "cardhome/1.webp",
+  //     huespedes: 16,
+  //     llegada: "14 Oct 2022",
+  //     salida: "17 Oct 2022",
+  //     fecha: "25 Sept 2022",
+  //     codigo: "HM22ASCE42",
+  //   },
+  // ];
+
   return (
     <div className="CardHomeContainer">
-      {data.map((item, index) => {
-        return (
-          <div key={index}>
-            <CardHomeHost
-              name={item.name}
-              date={item.fechas}
-              img={item.img}
-              huespedes={item.huespedes}
-              llegada={item.llegada}
-              salida={item.salida}
-              codigo={item.codigo}
-            />
-          </div>
-        );
-      })}
+      {reservations.length === 0 ? (
+        <h1>No reservations found, get ready to be the greatest host</h1>
+
+      ) : (
+        reservations.map((item, index) => {
+          return (
+            <div key={index}>
+              <CardHomeHost
+                name={item.user.name}
+                date={item.fechas}
+                img={item.user.profileimg}
+                huespedes={item.guests.adults}
+                llegada={item.fechas}
+                salida={item.fechas}
+                codigo={item._id}
+              />
+            </div>
+          );
+        })
+      )}
     </div>
   );
 };
--- src/pages/HostingListing.js
+++ src/pages/HostingListing.js
@@ -1,11 +1,74 @@
-import '../styles/components/HostingListing.scss'
+import "../styles/components/HostingListing.scss";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import ListingContainer from "../components/ListingContainer";
 
-const HostingListing = ()=>{
-    return(
-        <div>
+const HostingListing = () => {
+  const [loading, setLoading] = useState(true);
+  const [homes, setHomes] = useState("");
+  const token = localStorage.getItem("token");
 
-        </div>
-    )
-}
+  const getHomes = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://airbnbclonetop24.herokuapp.com/homes/listings",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setHomes(data.data.homes);
+      setLoading(false);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    getHomes();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="hostlistings">
+      <div className="hostlistings__title">
+        <h1>{`${homes.length} anuncios`}</h1>
+        <Link to="/becomehost">+ Crea un anuncio</Link>
+      </div>
+      <div className="listingcontainer" style={{color:'grey', borderTop:'none'}}>
+        <div className="listingcontainer__img">Anuncio</div>
+        <div>Ubicacion</div>
+        <div>Capacidad</div>
+        <div>Cuartos</div>
+        <div>Calificacion</div>
+        <div>Precio</div>
+      </div>
+      {homes.length === 0 ? (
+        <h1>No homes found</h1>
+      ) : (
+        homes.map((item) => {
+          return (
+            <>
+              <ListingContainer
+                img={item.images[0]}
+                location={item.location.city}
+                capacity={item.capacity}
+                rooms={item.rooms}
+                score={item.scorevalue}
+                price={item.price}
+              />
+            </>
+          );
+        })
+      )}
+    </div>
+  );
+};
 
 export default HostingListing;
